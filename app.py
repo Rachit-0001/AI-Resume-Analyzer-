68d2cd3ac814a88feb63f43739178dded2435fd4
Rachit Gangwar <156359363+Rachit-0001@users.noreply.github.com>
2026-04-09 00:06:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from utils.parser import extract_text
 from utils.analyzer import analyze_resume
 import os
 
-
-    
 app = Flask(__name__)
+
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,11 +17,15 @@ def index():
         file = request.files['resume']
         role = request.form.get('role')
 
-        resume_text = extract_text(file)
-        result = analyze_resume(resume_text, role)
+        if file:
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+
+            resume_text = extract_text(file_path)   
+            result = analyze_resume(resume_text, role)
 
     return render_template('index.html', result=result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  
     app.run(host="0.0.0.0", port=port)
